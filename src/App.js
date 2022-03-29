bf84b6ae4b39575ad1ee6a8639737937393ddf93
Umairnaim10 <97894886+Umairnaim10@users.noreply.github.com>
2022-03-29 19:27:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+import Header from "./Components/NavBar";
+import SearchBar from "./Components/SearchBar";
+import CharacterList from "./Components/CharacterList";
+import Spinner from "./Components/Spinner";
+import { useState,useEffect } from 'react';
+import axios from "./api";
+
+function App() {
+  const [items,setItems] = useState([]);
+  const [loading,setLoading] = useState(true);
+  const [query,setQuery] = useState("");
+
+
+  useEffect (()=>{
+    const fetchItems = async () => {
+      const results = await axios.get(`./characters?name=${query}`);
+      setItems(results.data)
+      setLoading(false)
+  };
+  fetchItems();
+  },[query]);
+
+  return (
+   <>
+   <div className="app">
+   <Header/>
+   <SearchBar setQuery={(query)=>setQuery(query)}/>
+   {loading ? <Spinner/> : <CharacterList items = {items}/> }
+   </div>
+   </>
+    
+  );
+}
+
+export default App;
